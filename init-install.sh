f09707d0485e4d79f3d95805924a8a20df39332e
stefa2k <stefan.kobrc@rocklogic.at>
2021-03-20 20:46:19 +0100

    configurable api bind address

--- init-install.sh
+++ init-install.sh
@@ -39,6 +39,7 @@ network: $e2dc_network
 setup: $e2dc_client
 setup_override: $e2dc_override
 eth1_node: $eth1_node
+api_bind_address: 127.0.0.1
 
 # mapping table (key, value) with network name as key and branch name as value
 networks:
